2113b3dc85cfe950eb46315a8f8da0c7b9cd7934
mykolachupyak <chupyak@gmail.com>
2022-12-26 15:49:13 +0200

    committe

--- src/property_components/image-component/ImageComponent.tsx
+++ src/property_components/image-component/ImageComponent.tsx
@@ -4,8 +4,7 @@ import PropertyPhoto from "../../images/flat1.jpg";
 
 export default function ImageComponent() {
     return (
-        // <a href={`description`}><img className="img-picture" src={PropertyPhoto} alt="PropertyPhoto"/></a>
         <a target="_blank" href={`description`} rel="noreferrer"><img className="img-picture" src={PropertyPhoto} alt="PropertyPhoto"/></a>
-);
+    );
 }
 
